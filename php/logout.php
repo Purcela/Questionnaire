7c46586fa5dd7d8f110c87242ef74517bdf76c91
Giorgi phurtseladze <99269771+Purcela@users.noreply.github.com>
2025-08-18 14:51:35 +0400

    Revert "Questionnaire"
    This reverts commit 1c7cfab9c71331fcc09c241f02fbcd538f018d23.

--- php/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start(); // სესიის დაწყება
-session_unset(); // ყველა სესიის ცვლადის გასუფთავება
-session_destroy(); // სესიის განადგურება
-
-header("Location: ../register.php"); // გადამისამართება ავტორიზაციის გვერდზე
-exit();
-?>
